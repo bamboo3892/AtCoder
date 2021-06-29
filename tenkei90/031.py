53850037a15bc14d17552599fa22342a4ce24aab
bamboo3892 <applebanana137@gmail.com>
2021-06-29 19:13:52 +0900

    20210629

--- tenkei90/031.py
+++ tenkei90/031.py
@@ -8,12 +8,36 @@ def main():
 
     ans = 0
     for n in range(N):
-        ans += W[n]
-        b2 = B[n] + W[n] * (W[n] + 1) // 2
-        if(b2 >= 2):
-            ans += int(math.log(b2 - 1, 2)) + 1
+        ans ^= calc_grundy(W[n], B[n])
 
-    print("First" if ans % 2 else "Second")
+    print("Second" if ans == 0 else "First")
+
+
+grundy = [[None] * 1326 for _ in range(51)]  # 0:必敗, 1以上:必勝
+
+
+def calc_grundy(w, b):
+    if(grundy[w][b] is not None):
+        return grundy[w][b]
+    else:
+        if(w == 0 and b in [0, 1]):
+            grundy[w][b] = 0
+        else:
+            g = set()
+            if(w != 0):
+                g.add(calc_grundy(w - 1, b + w))
+            for sub_b in range(1, b // 2 + 1):
+                g.add(calc_grundy(w, b - sub_b))
+            grundy[w][b] = mex(g)
+        return grundy[w][b]
+
+
+def mex(s):
+    l = [-1] + sorted(list(s))
+    for i in range(len(l) - 1):
+        if(l[i] + 1 != l[i + 1]):
+            return l[i] + 1
+    return l[-1] + 1
 
 
 if(__name__ == "__main__"):
